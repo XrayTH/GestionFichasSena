4e4ea2d7053afa43d4402451677db06667b0f1e4
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-09-26 20:01:33 -0500

    Interfaz de gestion de usuarios v0.1.1

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -23,134 +23,161 @@ const UserComponent = () => {
   return (
     <div className={classes.container}>
       {/* 1. Usuario */}
-      <label className={classes.label}>Usuario:</label>
-      <TextField
-        className={classes.textField}
-        value={formState.username}
-        variant="outlined"
-        InputProps={{
-          readOnly: !isEditable,
-        }}
-      />
-
-      {/* 2. Contraseña */}
-      <label className={classes.label}>Contraseña:</label>
-      <div className={classes.passwordWrapper}>
+      <div className={classes.fieldContainer}>
+        <label className={classes.label}>Usuario:</label>
         <TextField
           className={classes.textField}
-          type={showPassword ? 'text' : 'password'}
-          value={formState.password}
+          value={formState.username}
           variant="outlined"
           InputProps={{
             readOnly: !isEditable,
           }}
         />
-        <Button
-          onMouseDown={() => setShowPassword(true)}
-          onMouseUp={() => setShowPassword(false)}
-          className={classes.button}
-        >
-          👁️
-        </Button>
+      </div>
+
+      {/* 2. Contraseña */}
+      <div className={classes.fieldContainer}>
+        <label className={classes.label}>Contraseña:</label>
+        <div className={classes.passwordWrapper}>
+          <TextField
+            className={classes.textField}
+            type={showPassword ? 'text' : 'password'}
+            value={formState.password}
+            variant="outlined"
+            InputProps={{
+              readOnly: !isEditable,
+            }}
+          />
+          <Button
+            onMouseDown={() => setShowPassword(true)}
+            onMouseUp={() => setShowPassword(false)}
+            className={classes.button}
+          >
+            👁️
+          </Button>
+        </div>
       </div>
 
       {/* 3. Rol */}
-      <label className={classes.label}>Rol:</label>
-      <TextField
-        className={classes.textField}
-        value={formState.role}
-        variant="outlined"
-        InputProps={{
-          readOnly: !isEditable,
-        }}
-      />
+      <div className={classes.fieldContainer}>
+        <label className={classes.label}>Rol:</label>
+        <TextField
+          className={classes.textField}
+          value={formState.role}
+          variant="outlined"
+          InputProps={{
+            readOnly: !isEditable,
+          }}
+        />
+      </div>
 
       {/* 4. Editar */}
-      <label className={classes.label}>Editar:</label>
-      <Switch
-        className={classes.switch}
-        checked={formState.canEdit}
-        disabled={!isEditable}
-        onChange={() =>
-          setFormState({ ...formState, canEdit: !formState.canEdit })
-        }
-      />
+      <div className={classes.fieldContainer}>
+        <label className={classes.label}>Editar:</label>
+        <Switch
+          className={classes.switch}
+          checked={formState.canEdit}
+          disabled={!isEditable}
+          onChange={() =>
+            setFormState({ ...formState, canEdit: !formState.canEdit })
+          }
+        />
+      </div>
 
       {/* 5. Crear */}
-      <label className={classes.label}>Crear:</label>
-      <Switch
-        className={classes.switch}
-        checked={formState.canCreate}
-        disabled={!isEditable}
-        onChange={() =>
-          setFormState({ ...formState, canCreate: !formState.canCreate })
-        }
-      />
+      <div className={classes.fieldContainer}>
+        <label className={classes.label}>Crear:</label>
+        <Switch
+          className={classes.switch}
+          checked={formState.canCreate}
+          disabled={!isEditable}
+          onChange={() =>
+            setFormState({ ...formState, canCreate: !formState.canCreate })
+          }
+        />
+      </div>
 
       {/* 6. Gestionar Usuarios */}
-      <label className={classes.label}>Gestionar Usuarios:</label>
-      <Switch
-        className={classes.switch}
-        checked={formState.canManageUsers}
-        disabled={!isEditable}
-        onChange={() =>
-          setFormState({
-            ...formState,
-            canManageUsers: !formState.canManageUsers,
-          })
-        }
-      />
+      <div className={classes.fieldContainer}>
+        <label className={classes.label}>Gestionar Usuarios:</label>
+        <Switch
+          className={classes.switch}
+          checked={formState.canManageUsers}
+          disabled={!isEditable}
+          onChange={() =>
+            setFormState({
+              ...formState,
+              canManageUsers: !formState.canManageUsers,
+            })
+          }
+        />
+      </div>
 
       {/* 7. Botones */}
-      <Button
-        className={classes.button}
-        onClick={handleEditClick}
-      >
-        {isEditable ? 'Guardar' : 'Editar'}
-      </Button>
-      <Button className={classes.deleteButton}>Borrar</Button>
+      <div className={classes.buttonContainer}>
+        <Button
+          className={classes.button}
+          onClick={handleEditClick}
+        >
+          {isEditable ? 'Guardar' : 'Editar'}
+        </Button>
+        <Button className={classes.deleteButton}>Borrar</Button>
+      </div>
     </div>
   )
 }
 
 const useStyles = makeStyles(() => ({
-    container: {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(7, 1fr)',
-      gridGap: '10px',
-      backgroundColor: '#f5f5f5',
-      padding: '20px',
-      borderRadius: '8px',
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(7, 1fr)',
+    gridGap: '10px',
+    backgroundColor: '#f5f5f5',
+    padding: '20px',
+    borderRadius: '8px',
+  },
+  fieldContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gridColumn: 'span 7', // Ocupa las 7 columnas
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#2914ae',
+    alignSelf: 'flex-start', // Alinea las etiquetas al inicio
+    marginBottom: '5px',
+  },
+  textField: {
+    width: '100%',
+  },
+  passwordWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#2914ae',
+    color: '#fff',
+    marginTop: '10px', // Espaciado superior para los botones
+    '&:hover': {
+      backgroundColor: '#4cae14',
     },
-    label: {
-      fontWeight: 'bold',
-      color: '#2914ae',
-      alignSelf: 'center',
+  },
+  deleteButton: {
+    backgroundColor: '#ae1499',
+    color: '#fff',
+    marginTop: '10px', // Espaciado superior para el botón de borrar
+    '&:hover': {
+      backgroundColor: '#d81b60',
     },
-    textField: {
-      width: '100%',
-    },
-    passwordWrapper: {
-      display: 'flex',
-      alignItems: 'center',
-    },
-    button: {
-      backgroundColor: '#2914ae',
-      color: '#fff',
-      '&:hover': {
-        backgroundColor: '#4cae14',
-      },
-    },
-    deleteButton: {
-      backgroundColor: '#ae1499',
-      color: '#fff',
-      '&:hover': {
-        backgroundColor: '#d81b60',
-      },
-    },
-    switch: {
-      alignSelf: 'center',
-    }
-  }))
-  
+  },
+  switch: {
+    alignSelf: 'center',
+  },
+}))
+
 export default UserComponent
