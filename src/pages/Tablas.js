1accefcec89b2beb6428c3f58f0f500f94138b7d
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-09-29 15:30:37 -0500

    Componente Tablas

--- src/pages/Tablas.js
+++ src/pages/Tablas.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import GesFicha from '../src/components/GestionFichas';
+import GesCoo from '../src/components/GestionCoordinadores';
+import GesIns from '../src/components/GestionInstructores';
 
-const Tablas = ( {tabla} ) => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Tablas = ({ tabla }) => {
+  switch (tabla) {
+    case 'Fichas':
+      return <div><GesFicha /></div>;
+    case 'Coordinadores':
+      return <div><GesCoo /></div>;
+    case 'Instructores':
+      return <div><GesIns /></div>;
+    case 'Programa':
+      return null;
+    default:
+      return <div>Tabla no encontrada</div>;
+  }
+};
 
-export default Tablas
+export default Tablas;
