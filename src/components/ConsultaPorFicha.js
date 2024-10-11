af68a2574981faddbdcf4d5f399c877f6da76675
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-10-11 18:14:16 -0500

    Consulyas por ficha v1.1

--- src/components/ConsultaPorFicha.js
+++ src/components/ConsultaPorFicha.js
@@ -3,11 +3,10 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { getAsignacionesByFicha } from '../service/asignacionService';
-import { Tooltip, Paper, Typography } from '@mui/material';
+import { Tooltip, Paper, Typography, Box } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { useLocation } from 'react-router-dom';
 
-//Mapeo de dias de la semana
 const daysMap = {
   Lunes: 1,
   Martes: 2,
@@ -18,34 +17,50 @@ const daysMap = {
   Domingo: 0,
 };
 
+const colorScale = [
+  '#FFFF00',
+  '#FFD700',
+  '#FF4500',
+  '#FF0000',
+  '#8B0000',
+  '#0000FF',
+  '#00008B',
+];
+
 const ConsultaPorFicha = () => {
   const location = useLocation();
   const { ficha } = location.state || {};
   const classes = useStyles();
   const localizer = momentLocalizer(moment);
   const [asignaciones, setAsignaciones] = useState([]);
+  const [jornadaColors, setJornadaColors] = useState({});
 
   useEffect(() => {
     const fetchAsignaciones = async () => {
       try {
         const response = await getAsignacionesByFicha(ficha.codigo);
         const formattedAsignaciones = [];
+        const colorsMap = {};
 
-        response.forEach((asignacion) => {
-          const start = moment(asignacion.inicio); 
-          const end = moment(asignacion.fin); 
-          const dayOfWeek = daysMap[asignacion.dia];  //Dia de la semana que debe repetirse
+        response.forEach((asignacion, index) => {
+          const start = moment(asignacion.inicio);
+          const end = moment(asignacion.fin);
+          const dayOfWeek = daysMap[asignacion.dia];
 
-          //Iteramos sobre el rango de fechas entre inicio y fin
+          if (!colorsMap[asignacion.jornada]) {
+            const colorIndex = Object.keys(colorsMap).length % colorScale.length;
+            colorsMap[asignacion.jornada] = colorScale[colorIndex];
+          }
+
           let currentDate = moment(start);
           while (currentDate.isSameOrBefore(end)) {
-            //Si el dia de la semana coincide con el de la asignacion
             if (currentDate.day() === dayOfWeek) {
               formattedAsignaciones.push({
                 title: asignacion.instructor,
                 start: currentDate.toDate(),
                 end: currentDate.toDate(),
                 jornada: asignacion.jornada,
+                color: colorsMap[asignacion.jornada],
               });
             }
             currentDate.add(1, 'days');
@@ -53,6 +68,7 @@ const ConsultaPorFicha = () => {
         });
 
         setAsignaciones(formattedAsignaciones);
+        setJornadaColors(colorsMap);
       } catch (error) {
         console.error('Error al cargar las asignaciones', error);
       }
@@ -60,9 +76,15 @@ const ConsultaPorFicha = () => {
     fetchAsignaciones();
   }, [ficha]);
 
-  const eventStyleGetter = () => {
+  const eventStyleGetter = (event) => {
     return {
-      className: classes.eventStyle,
+      style: {
+        backgroundColor: event.color,
+        color: '#ffffff',
+        padding: '5px',
+        borderRadius: '4px',
+        cursor: 'pointer',
+      },
     };
   };
 
@@ -84,6 +106,17 @@ const ConsultaPorFicha = () => {
 
   return (
     <div className={classes.calendarContainer}>
+      <Box className={classes.legendContainer}>
+        <Box className={classes.legendItems}>
+          {Object.entries(jornadaColors).map(([jornada, color]) => (
+            <Box key={jornada} className={classes.legendItem}>
+              <Box style={{ backgroundColor: color, width: '10px', height: '20px', borderRadius: '4px', marginRight: '10px' }}></Box>
+              <Typography variant="body2">{jornada}</Typography>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+
       <Calendar
         localizer={localizer}
         events={asignaciones}
@@ -103,8 +136,8 @@ const ConsultaPorFicha = () => {
 
 const useStyles = makeStyles(() => ({
   eventStyle: {
-    backgroundColor: '#1976d2', // Azul personalizado
-    color: '#ffffff', // Texto blanco
+    backgroundColor: '#1976d2',
+    color: '#ffffff',
     padding: '5px',
     borderRadius: '4px',
     cursor: 'pointer',
@@ -116,6 +149,22 @@ const useStyles = makeStyles(() => ({
   calendarContainer: {
     height: '100vh',
     overflowY: 'auto',
+  },
+  legendContainer: {
+    padding: '10px',
+    marginBottom: '20px',
+    border: '1px solid #ddd',
+    borderRadius: '8px',
+    backgroundColor: '#f9f9f9',
+  },
+  legendItems: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  legendItem: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '5px',
   },
 }));
 
