b18bac9a5a299491c2fc291b52e3d2f53eb0398c
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-10-11 19:08:57 -0500

    Consultas ficha v1.1.1

--- src/components/ConsultaPorFicha.js
+++ src/components/ConsultaPorFicha.js
@@ -6,6 +6,9 @@ import { getAsignacionesByFicha } from '../service/asignacionService';
 import { Tooltip, Paper, Typography, Box } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { useLocation } from 'react-router-dom';
+import 'moment/locale/es';
+
+moment.locale('es'); 
 
 const daysMap = {
   Lunes: 1,
@@ -42,7 +45,7 @@ const ConsultaPorFicha = () => {
         const formattedAsignaciones = [];
         const colorsMap = {};
 
-        response.forEach((asignacion, index) => {
+        response.forEach((asignacion) => {
           const start = moment(asignacion.inicio);
           const end = moment(asignacion.fin);
           const dayOfWeek = daysMap[asignacion.dia];
@@ -61,6 +64,8 @@ const ConsultaPorFicha = () => {
                 end: currentDate.toDate(),
                 jornada: asignacion.jornada,
                 color: colorsMap[asignacion.jornada],
+                asignacionInicio: asignacion.inicio, // Agregado para el tooltip
+                asignacionFin: asignacion.fin, // Agregado para el tooltip
               });
             }
             currentDate.add(1, 'days');
@@ -94,8 +99,8 @@ const ConsultaPorFicha = () => {
         <Paper className={classes.tooltip}>
           <Typography variant="body2">Instructor: {event.title}</Typography>
           <Typography variant="body2">Jornada: {event.jornada}</Typography>
-          <Typography variant="body2">Inicio: {moment(event.start).format('LL')}</Typography>
-          <Typography variant="body2">Fin: {moment(event.end).format('LL')}</Typography>
+          <Typography variant="body2">Inicio: {moment(event.asignacionInicio).format('LL')}</Typography>
+          <Typography variant="body2">Fin: {moment(event.asignacionFin).format('LL')}</Typography>
         </Paper>
       }
       arrow
@@ -106,14 +111,29 @@ const ConsultaPorFicha = () => {
 
   return (
     <div className={classes.calendarContainer}>
-      <Box className={classes.legendContainer}>
-        <Box className={classes.legendItems}>
-          {Object.entries(jornadaColors).map(([jornada, color]) => (
-            <Box key={jornada} className={classes.legendItem}>
-              <Box style={{ backgroundColor: color, width: '10px', height: '20px', borderRadius: '4px', marginRight: '10px' }}></Box>
-              <Typography variant="body2">{jornada}</Typography>
-            </Box>
-          ))}
+      <Box className={classes.flexContainer}>
+        <Box className={classes.legendContainer}>
+          <Box className={classes.legendItems}>
+            {Object.entries(jornadaColors).map(([jornada, color]) => (
+              <Box key={jornada} className={classes.legendItem}>
+                <Box style={{ backgroundColor: color, width: '10px', height: '20px', borderRadius: '4px', marginRight: '5px' }}></Box>
+                <Typography variant="body2" className={classes.legendText}>{jornada}</Typography>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+
+        <Box className={classes.infoContainer}>
+          <Typography variant="body2">Ficha: {ficha.codigo}</Typography>
+          <Typography variant="body2">Coordinador: {ficha.coordinador}</Typography>
+          <Typography variant="body2">Programa: {ficha.programa}</Typography>
+          <Typography variant="body2">Gestor: {ficha.gestor}</Typography>
+          <Typography variant="body2">Ambiente: {ficha.ambiente}</Typography>
+          <Typography variant="body2">Municipio: {ficha.municipio}</Typography>
+          <Typography variant="body2">Ubicación: {ficha.ubicacionGPS}</Typography>
+          <Typography variant="body2">Inicio: {ficha.inicio}</Typography>
+          <Typography variant="body2">Fin: {ficha.fin}</Typography>
+          <Typography variant="body2">Requerimientos: {ficha.requerimientos}</Typography>
         </Box>
       </Box>
 
@@ -127,32 +147,38 @@ const ConsultaPorFicha = () => {
         components={{
           event: EventComponent,
         }}
-        views={['month', 'week', 'day']}
+        views={['month']}
         popup
+        messages={{
+          allDay: 'Todo el día',
+          previous: 'Atrás',
+          next: 'Siguiente',
+          today: 'Hoy',
+          month: 'Mes',
+          week: 'Semana',
+          day: 'Día',
+          agenda: 'Agenda',
+          date: 'Fecha',
+          time: 'Hora',
+          event: 'Evento',
+          noEventsInRange: 'No hay eventos en este rango.',
+          showMore: (total) => `+ Ver más (${total})`,
+        }}
       />
     </div>
   );
 };
 
 const useStyles = makeStyles(() => ({
-  eventStyle: {
-    backgroundColor: '#1976d2',
-    color: '#ffffff',
-    padding: '5px',
-    borderRadius: '4px',
-    cursor: 'pointer',
-  },
-  tooltip: {
-    maxWidth: 250,
-    fontSize: '14px',
-  },
-  calendarContainer: {
-    height: '100vh',
-    overflowY: 'auto',
+  flexContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: '20px',
   },
   legendContainer: {
-    padding: '10px',
-    marginBottom: '20px',
+    padding: '5px',
+    marginRight: '20px',
     border: '1px solid #ddd',
     borderRadius: '8px',
     backgroundColor: '#f9f9f9',
@@ -164,7 +190,24 @@ const useStyles = makeStyles(() => ({
   legendItem: {
     display: 'flex',
     alignItems: 'center',
-    marginBottom: '5px',
+    marginBottom: '3px',
+  },
+  legendText: {
+    fontSize: '12px',
+  },
+  infoContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    fontSize: '12px',
+  },
+  tooltip: {
+    maxWidth: 250,
+    fontSize: '12px',
+  },
+  calendarContainer: {
+    height: '100vh',
+    overflowY: 'auto',
   },
 }));
 
