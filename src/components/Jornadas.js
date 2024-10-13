fe72101b26afc40e2715ff0e8439a5ec5816e27c
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-10-13 17:31:34 -0500

    Permisos v3

--- src/components/Jornadas.js
+++ src/components/Jornadas.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux'; // Importar useSelector
+import { selectUserPermisos } from '../features/userSlice'; // Importar el selector de permisos
 import { getJornadas, createJornada, updateJornadaById, deleteJornadaById } from '../service/jornadaService';
 import { makeStyles } from '@mui/styles';
 
 const Jornadas = () => {
   const classes = useStyles();
+
+  // Obtener los permisos del usuario desde el estado de Redux
+  const permisos = useSelector(selectUserPermisos);
+
   const [jornadas, setJornadas] = useState([]);
   const [selectedJornadaId, setSelectedJornadaId] = useState('');
   const [jornadaName, setJornadaName] = useState('');
@@ -20,6 +26,11 @@ const Jornadas = () => {
 
     fetchJornadas();
   }, []);
+
+  // Comprobar si el usuario tiene permiso para ver "tablas"
+  if (!permisos.tablas) {
+    return null; // Si no tiene permiso, no renderiza nada
+  }
 
   const handleCreate = async () => {
     try {
@@ -96,10 +107,10 @@ const Jornadas = () => {
 const useStyles = makeStyles(() => ({
   container: {
     display: 'flex',
-    flexWrap: 'wrap', // Permite que los elementos se envuelvan en varias líneas
+    flexWrap: 'wrap',
     alignItems: 'center',
     marginBottom: '1rem',
-    gap: '1rem', // Espacio entre los elementos
+    gap: '1rem',
   },
   input: {
     padding: '0.5rem',
@@ -127,11 +138,11 @@ const useStyles = makeStyles(() => ({
   },
   '@media (max-width: 600px)': {
     container: {
-      flexDirection: 'column', // Cambia a columnas cuando la pantalla es más pequeña
+      flexDirection: 'column',
       alignItems: 'stretch',
     },
     button: {
-      width: '100%', // Los botones ocupan todo el ancho disponible
+      width: '100%',
     },
   },
 }));
