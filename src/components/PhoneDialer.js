03f7369fb82986a570b33fc8dbd3f1afedd3c6cf
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-10-19 13:28:15 -0500

    Llamar v0.1

---
+++ src/components/PhoneDialer.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import PhoneIcon from '@mui/icons-material/Phone';
+
+const isMobileDevice = () => {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  return /android|iphone|ipad|iPod/i.test(userAgent);
+};
+
+const PhoneDialer = ({ phoneNumber }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(isMobileDevice());
+  }, []);
+
+  const handlePhoneClick = () => {
+    const formattedNumber = `+57${phoneNumber}`;
+    window.location.href = `tel:${formattedNumber}`;
+  };
+
+  if (!isMobile) {
+    return null;
+  }
+
+  return (
+    <IconButton
+      color="primary"
+      onClick={handlePhoneClick}
+      sx={{
+        padding: 0.5, // Tamaño pequeño
+      }}
+    >
+      <PhoneIcon fontSize="small" />
+    </IconButton>
+  );
+};
+
+export default PhoneDialer;
